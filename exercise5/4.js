f14aa3be16631bfeaab32bd3f3b295c9213dfdd3
umarzumaro <umarzumaro5@gmail.com>
2019-08-21 12:35:31 +0700

    first

---
+++ exercise5/4.js
@@ -0,0 +1,19 @@
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word.substring(0,3);
+var secondWord = word.substring(4,14);
+var thirdWord = word.substring(15,17);
+var fourthWord = word.substring(18,20);
+var fifthWord = word.substring(21,25);
+
+var firstWordLength = exampleFirstWord.length;
+var secondWordLength = secondWord.length;
+var thirdWordLength = thirdWord.length;
+var fourthWordLength = fourthWord.length;
+var fifthWordLength = fifthWord.length;
+// create new variables around here
+
+console.log('First Word: ' + exampleFirstWord + ',with length :' +exampleFirstWord.length);
+console.log('Second Word: ' + secondWord + secondWord.length );
+console.log('Third Word: ' + thirdWord + thirdWord.length);
+console.log('Fourth Word: ' + fourthWord + fourthWord.length);
+console.log('Fifth Word: ' + fifthWord + fifthWord.length);
